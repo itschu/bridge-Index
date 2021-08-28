63b48baaed4d2217551c7a8a90d6ff4a166ec1cc
Joseph Chu <joseoh-chu@pop-os.localdomain>
2021-08-28 10:45:44 +0100

    extended count down timer

--- assets/js/coming.js
+++ assets/js/coming.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Aug 27, 2021 12:00:00").getTime();
+let launchDate = new Date("Sept 1, 2021 12:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
